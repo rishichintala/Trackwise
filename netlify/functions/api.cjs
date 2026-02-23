05be20e4b461455f1f453845e50ecbfb68eab1bf
Sai Rishith <rishi.chintala19@gmail.com>
2026-02-22 23:03:33 -0500

    fixed gemini bot comments

--- netlify/functions/api.cjs
+++ netlify/functions/api.cjs
@@ -1,5 +1,7 @@
 const serverless = require('serverless-http');
 const app = require('../../server/index');
 
+console.log('--- Netlify Function [api] Initialized ---');
+
 // Wrap the Express app in a serverless handler securely
 module.exports.handler = serverless(app);
